8248278c3a76e98d46aa37ba8d1ef3b18bda958a
Electronica Care <mohamedmahmoudzaz20@gmail.com>
2026-06-03 13:11:05 +0300

    fixed bug.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'firebase_options.dart';
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
